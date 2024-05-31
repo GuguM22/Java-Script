0639fc99841724112b6b9f24c7dbf9561ebb000d
GuguM22 <gugu.mkhwanazi40@gmail.com>
2024-05-31 14:07:20 +0200

    added javascript script

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
-let num1 = 12;
-let num2 = 2;
-let answer = num1+num2;
-console.log('your answer is: ' + 12);
+// let num1 = 12;
+// let num2 = 2;
+// let answer = num1+num2;
+// console.log('your answer is: ' + 12);
+
+
+op = prompt('Enter your operator')
+if (op==='+') 
+    {console.log ('your answer is' + (num1+num2))
+
+
+    }else if (op ==='-'){
+        console.log(num1-num2)
+    }
+
+    else if (op ==='*')
+        {console.log(num1 *num2)}
+
+    else if (op ==='/')
+        {console.log(num1 / num2)}
+
+    else { console.log ('invalid operator')}
+
