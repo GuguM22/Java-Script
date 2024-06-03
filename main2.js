a8ad1c1c4a5230087a7b856fbef772282ae2a8e8
GuguM22 <gugu.mkhwanazi40@gmail.com>
2024-06-03 12:02:12 +0200

    firstjavaproject

--- main2.js
+++ main2.js
@@ -2,67 +2,52 @@
 
 
 
-// let num1 = prompt ('enter your number');
-// let num2 = 2;
-// let answer = num1+num2;
-
-// op = prompt ('enter num1');
-// if (op==='+') 
-
-
-
-// prompt= ('num2');
-// console.log ('your answer is') + (num1 + num2) 
-
-
-// let num1 = number (prompt('Enter first your num'));
-
-// let op = prompt ('enter your operator');
-// let num2 = number (prompt('Enter  your second num'));
-
-
-
-// // op = prompt('Enter your operator')
-// if (op==='+') 
-//     {console.log ('your answer is' + (num1 + num2))
-
-
-//     }else if (op ==='-'){
-//         console.log(num1-num2)
-//     }
-
-//     else if (op ==='*')
-//         {console.log(num1 *num2)}
-
-//     else if (op ==='/')
-//         {console.log(num1 / num2)}
-
-
-//     else { console.log ('invalid operator')}
 
 
 
 
 
 
+let num1 = prompt ('enter your number');
+let num2 = 2;
+let answer = num1+num2;
+
+op = prompt ('enter num1');
+if (op==='+') 
 
 
-let Fname = prompt('Enter your Name');
-let age = Number(prompt('Enter your Age'));
-let citizenship = prompt('Are you South African ?');
-let greeting = 'Hi ';
-console.log(greeting + Fname);
+
+prompt= ('num2');
+console.log ('your answer is') + (num1 + num2) 
 
 
-if (age < 18) {
-    console.log("You can't vote");
-} 
+let num1 = number (prompt('Enter first your num'));
 
-else if (citizenship !== 'Yes') { 
-    console.log('You can\'t vote');
-} else {
-    console.log('You can register to vote');
-}
+let op = prompt ('enter your operator');
+let num2 = number (prompt('Enter  your second num'));
+
+
+
+// op = prompt('Enter your operator')
+if (op==='+') 
+    {console.log ('your answer is' + (num1 + num2))
+
+
+    }else if (op ==='-'){
+        console.log(num1-num2)
+    }
+
+    else if (op ==='*')
+        {console.log(num1 *num2)}
+
+    else if (op ==='/')
+        {console.log(num1 / num2)}
+
+
+    else { console.log ('invalid operator')}
+
+
+
 
 
 
